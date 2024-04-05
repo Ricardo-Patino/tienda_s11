f8967026b2a01004cd94de8d8d0c20cac7e8a1e5
Rick <77856968+Ricardo-Patino@users.noreply.github.com>
2024-04-05 09:51:18 -0600

    fixed issues
    fixed issues

--- tienda_s11/tienda_s11/src/main/java/com/tienda/service/impl/CorreoServiceImpl.java
+++ tienda_s11/tienda_s11/src/main/java/com/tienda/service/impl/CorreoServiceImpl.java
@@ -1,34 +1,36 @@
 package com.tienda.service.impl;
- 
+
 import com.tienda.service.CorreoService;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
  
 /**
 *
 * @author Racha
 */
-public class CorreoServiceImp implements CorreoService {
- 
-    @Autowired
+@Service
+public class CorreoServiceImpl implements CorreoService {
+     @Autowired
     private JavaMailSender mailSender;
- 
+
     @Override
-    public void enviarCorreoHtml(String para, String asunto, String contenidoHtml) throws MessagingException {
- 
+      public void enviarCorreoHtml(
+              String para, 
+              String asunto, 
+              String contenidoHtml) 
+              throws MessagingException {
+          
         MimeMessage message = mailSender.createMimeMessage();
-        MimeMessageHelper helper
-                = new MimeMessageHelper(message,
+        MimeMessageHelper helper 
+                = new MimeMessageHelper(message, 
                         true);
         helper.setTo(para);
         helper.setSubject(asunto);
-        helper.setText(contenidoHtml, true);
+        helper.setText(contenidoHtml,true);
         mailSender.send(message);
- 
-  
     }
- 
 }
